ec229f2bc91e89b00a64e9c45422f4abbe640996
wuchengxing <1449697757@qq.com>
2022-07-18 09:36:24 +0800

    feat：zookeeper简单的api使用

--- simple-schedule-test/src/main/java/com/simple/schedule/test/task/DemoTaskOne.java
+++ simple-schedule-test/src/main/java/com/simple/schedule/test/task/DemoTaskOne.java
@@ -1,6 +1,7 @@
 package com.simple.schedule.test.task;
 
 import com.simple.schedule.annotation.SimpleSchedule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,12 +15,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class DemoTaskOne {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @SimpleSchedule(cron = "0/3 * * * * *", desc = "01定时任务执行测试：taskMethod01", autoStartup = true)
     public void taskMethod01() {
         try {
             Thread.sleep(100);
         } catch (InterruptedException ignored) {
         }
-        System.out.println("01定时任务执行测试：taskMethod01");
+        System.out.println("port: " + port + "==01定时任务执行测试：taskMethod01");
     }
 }

2c481c0f3c4fdd3a1c26c44bc8212bbfdbe81715
wuchengxing <1449697757@qq.com>
2022-07-26 18:40:23 +0800

    feat：分库分表测试

---
+++ dbrouter-test/src/main/java/com/simple/middle/test/dbrouter/DbRouterApplication.java
@@ -0,0 +1,19 @@
+package com.simple.middle.test.dbrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目: pagination
+ * <p>
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2022-07-26 18:31
+ **/
+@SpringBootApplication
+public class DbRouterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbRouterApplication.class, args);
+    }
+}

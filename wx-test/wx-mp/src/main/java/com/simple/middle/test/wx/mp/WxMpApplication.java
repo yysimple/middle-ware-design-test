bd6e3e6cfd913a1827f0d129569f07338808ea8a
wuchengxing <1449697757@qq.com>
2022-08-19 16:48:37 +0800

    feat：wx相关的测试

---
+++ wx-test/wx-mp/src/main/java/com/simple/middle/test/wx/mp/WxMpApplication.java
@@ -0,0 +1,20 @@
+package com.simple.middle.test.wx.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目: pagination
+ * <p>
+ * 功能描述: 微信公众号启动类
+ *
+ * @author: WuChengXing
+ * @create: 2022-08-19 16:19
+ **/
+@SpringBootApplication
+public class WxMpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxMpApplication.class, args);
+    }
+}
